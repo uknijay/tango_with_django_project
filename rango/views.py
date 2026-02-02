72bd67c66ca166d6e4efa5fd1792f22ab3cb86dc
Jay Jayaprakash <jayavardhanjp@gmail.com>
2026-02-02 16:38:56 +0000

    Chapter 4 completed, all tests passed

--- rango/views.py
+++ rango/views.py
@@ -8,4 +8,6 @@ def index(request):
     return render(request, 'rango/index.html',context= context_dict)
 
 def about(request):
-    return HttpResponse('Rango says here is the about page.<a href="/rango/">Index</a>')
+    context_dict = {'boldmessage': 'This tutorial has been put together by Jay'}
+
+    return render(request, 'rango/about.html',context= context_dict)
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -18,6 +18,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 
 # Quick-start development settings - unsuitable for production
@@ -67,6 +68,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media'
             ],
         },
     },
@@ -125,3 +127,6 @@ USE_TZ = True
 STATICFILES_DIRS = [STATIC_DIR, ]
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
